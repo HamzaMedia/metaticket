63cc315f4cbc29a30c83b104b522a42375c35be9
Protogen <101275325+HamzaMedia@users.noreply.github.com>
2024-07-03 16:17:39 +0300

    Update and rename config (1).js to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'MTI1O0NjcxOA.G883Ay.43EKFRn2R7LQa535JghFvOWjkisR5O_MQbv1wk', // Bot token
+  token: 'BOT-TOKEN-BURAYA', // Bot token
   prefix: '!', // Komut prefix
   logChannelId: '1258035421481271378', // Log kanal ID
   ticketCategoryId: '1258035356478083134', // Ticket kategori ID
